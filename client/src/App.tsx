9b5e52bce4690b269871e9a1f6db4b0dfbce5307
akbarakma1234 <akbarajo1234@gmail.com>
2023-06-27 23:42:11 +0700

    deploy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -155,6 +155,7 @@ export default function App() {
       <div style={{ marginTop: 50 }}>
         <h4>Your Player Code: { randomCode }</h4>
         <h4>Status: { gameConnectedString ? 'Game is Connected': 'Game is Not Connected' }</h4>
+        <p>If the socket connection is not connected, try refreshing the web as the server use free service</p>
       </div>
       <br/>
       
